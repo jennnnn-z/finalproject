1a6f165a0b5b621fd81a3835f6fb6c301c85278f
Jennifer Zhang <jlz2@andrew.cmu.edu>
2022-12-05 13:08:37 -0500

    finished edit and delete functions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const url = 'http://localhost:3005/';
 
 var activities = [];
 $(document).ready(function(){
-  getAllFaves();
+  // getAllFaves();
   $("#getactivity").click(function(){
     $.getJSON(url + 'activity?type=' + type.value, (thing) => {
       $("#activity").empty();
@@ -18,9 +18,12 @@ $(document).ready(function(){
     });
   });
   
-  $("#list").click(function(){
-    $("#allfaves").toggle();
+  $("#refresh").click(function(){
+    // $("#allfaves").toggle();
+    getAllFaves();
   });
+
+  
 });
 
 function getAllFaves(){
@@ -30,14 +33,32 @@ function getAllFaves(){
   $.getJSON(url + 'favorites/', (faves) => {
     console.log(faves);
     faves.forEach((fave) => {
-      $("#allfaves").append("<div class=\"fave\" id=\""+ fave.key + "\">" + fave.activity + "&nbsp;<button id=\""+ fave.key + "\">Delete</button>"+"</div>");
-      $("body").on('click', 'button#' + fave.key, (t)=>{
+      $("#allfaves").append("<div style=\"display:inline;\" id=\""+ fave.key + "\">" + fave.activity + "&nbsp;</div>");
+      // $("#allfaves").append("<p><code>data-ma-enter='submit'</code><span data-malleable='true' data-ma-blur='submit'>" + fave.activity + "</span></p>");
+      // $("#allfaves").append("<a href=\"#\" id=\"username\" data-type=\"text\" data-pk="1" data-title=\"Enter username\" class=\"editable editable-click\" style=\"display: inline; background-color: rgba(0, 0, 0, 0);\">superuse</a>");
+      $("div#" + fave.key).append("<button class=\"edit\" id=\""+ fave.key + "\">Edit</button>&nbsp;");
+      $("div#" + fave.key).append("<button class=\"delete\" id=\""+ fave.key + "\">Delete</button><br>");
+      $("body").on('click', 'button#' + fave.key + ".delete", (t)=>{
         console.log("calling DELETE on " + t.currentTarget.id);
         deleteThing(t.currentTarget.id);
-        // remove all elements with the id of fave.key
-        $("div").remove("#" + fave.key);
       });
-    });
+      $("body").on('click', 'button#' + fave.key + ".edit", (t)=>{
+        console.log("calling PUT on " + t.currentTarget.id + ": " + fave.activity);
+        let newActivity = prompt("Enter new activity", fave.activity);
+        console.log(newActivity);
+        editFave(t.currentTarget.id, newActivity);
+        // prompt("Edit Activity", fave.activity, (activity) => {
+        //   editFave(t.currentTarget.id, activity);
+        // }
+      });
+      // // on keypress enter run editFave
+      // $(".edit").on('keypress', '.edit', (t)=>{
+      //   if (t.which == 13){
+      //     console.log("pressed ENTER");
+      //     editFave(t.target.id, t.target.innerText);
+      //   }
+      // });
+      });
   });
 }
 
@@ -49,7 +70,7 @@ function addFave(key){
     type: 'POST',
     success: (res) => {
       console.log('Success', res);
-      getAllFaves();
+      // getAllFaves();
     }, 
     error: () => {
       // alert('Already Favorited!');
@@ -63,9 +84,70 @@ function deleteThing(key){
     type: 'DELETE',
     success: (res) => {
       console.log('Success', res);
+      // remove all items with the id of key 
+      $("div").remove("#" + key);
     }, 
     error: () => {
       alert('Already Deleted!');
     }
   })
 }
+
+// function divClicked() {
+//   var divHtml = $(this).text();
+//   var editableText = $("<textarea />");
+//   editableText.val(divHtml);
+//   $(this).replaceWith(editableText);
+//   editableText.focus();
+//   // setup the blur event for this new textarea
+//   editableText.blur(editableTextBlurred);
+// }
+
+// function editableTextBlurred() {
+//   var html = $(this).val();
+//   var viewableText = $("<div>");
+//   viewableText.html(html);
+//   $(this).replaceWith(viewableText);
+//   // setup the click event for this new div
+//   $(viewableText).click(divClicked);
+// }
+
+function editFave(key, activity){
+  console.log('editing fave...' + key + ": " + activity);
+  const body = {
+    'key': key,
+    'activity': activity
+  };
+  $.ajax({
+    url: url + 'favorite/update/',
+    type: 'PUT',
+    data: JSON.stringify(body),
+    dataType: 'json',
+    contentType: 'application/json',
+    // success: (res) => {
+    //   console.log('Success', res);
+    // }, 
+    // error: () => {
+    //   console.log("sus");
+    //   // alert(JSON.stringify(body));
+    //   // alert('Already Favorited!');
+    // }
+  })
+}
+
+// // now create the malle
+// const malle = new Malle({
+//   // this is the function that will be called once user has finished entering text (press Enter or click outside)
+//   // it receives the new value, the original element, the event and the input element
+//   fun: (value, original, event, input) => {
+//     console.log(`New text: ${value}`);
+//     console.log(`Original element:`);
+//     console.log(original);
+//     // add here your code for POSTing the new value
+//     // something along the line of:
+//     return fetch('/ajax', {
+//       method: 'POST',
+//       body: JSON.stringify({ 'name': value, 'id': original.dataset.id }),
+//     });
+//   },
+// }).listen(); // directly start listening after object creation
